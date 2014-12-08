ef213308651d31568c4f60f3d155e7ffab47e430
artem <artem.valeryvich@gmail.com>
2014-12-08 22:35:37 +0300

    last

--- src/ru/fizteh/fivt/students/artem_gritsay/DataBaseException.java
+++ src/ru/fizteh/fivt/students/artem_gritsay/DataBaseException.java
@@ -2,10 +2,6 @@ package ru.fizteh.fivt.students.artem_gritsay;
 
 import java.io.IOException;
 
-/**
- * Created by artem on 08.12.14.
- */
-
 public class DataBaseException extends IOException {
 
         public DataBaseException(String message, Exception cause) {
