b1c75d8e460028540599fafc856a9a8cc25fdb15
artem <artem.valeryvich@gmail.com>
2014-12-08 22:28:34 +0300

    last3

--- src/ru/fizteh/fivt/students/artem_gritsay/myDataBaseException.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.artem_gritsay;
-
-import java.io.IOException;
-
-public class MyDataBaseException extends IOException {
-    public MyDataBaseException(String message, Exception cause) {
-        super(message, cause);
-    }
-}
