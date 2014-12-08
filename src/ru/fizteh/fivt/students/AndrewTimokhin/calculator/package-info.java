5f13a1fa480ee03f9beb068917fd8e96e72714e0
artem <artem.valeryvich@gmail.com>
2014-12-08 22:26:13 +0300

    last2

---
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/calculator/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by artem on 08.12.14.
+ */
+package ru.fizteh.fivt.students.AndrewTimokhin.calculator;
--- src/ru/fizteh/fivt/students/artem_gritsay/myDataBaseException.java
+++ src/ru/fizteh/fivt/students/artem_gritsay/myDataBaseException.java
@@ -2,7 +2,6 @@ package ru.fizteh.fivt.students.artem_gritsay;
 
 import java.io.IOException;
 
-
 public class MyDataBaseException extends IOException {
     public MyDataBaseException(String message, Exception cause) {
         super(message, cause);
